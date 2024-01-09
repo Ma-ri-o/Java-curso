536b31c9d7e4ff794ab2b7c4b4f8ade49a06c5ee
Mario <mac@Marios-MacBook-Air.local>
2024-01-09 06:41:50 -0600

    adivina numero

---
+++ Primeros Pasos/Adivina_numero.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Adivina_numero {
+
+    public static void main(String[] args) {
+
+        int aleatorio=(int)(Math.random()*100);
+        Scanner entrada=new Scanner(System.in);
+        int numero=0;
+        int intentos=0;
+        while (numero!=aleatorio){
+            System.out.println("Introduce un número por favor");
+             numero=entrada.nextInt();
+             if(aleatorio<numero){
+                 System.out.println("Más bajo");
+             } else if ( aleatorio>numero) {
+                 System.out.println("Más alto");
+
+
+             }
+            System.out.println("correcto");
+
+
+        }
+
+
+
+
+    }
+
+}
